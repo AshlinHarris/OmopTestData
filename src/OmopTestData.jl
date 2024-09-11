835600569bfdf292c50e40e614ae9d5ff8d6d247
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-09-10 21:41:04 -0400

    Read in data from CSVs

--- src/OmopTestData.jl
+++ src/OmopTestData.jl
@@ -6,13 +6,16 @@ function main()
 	con = DBInterface.connect(DuckDB.DB, ":memory:")
 	pattern = r"CREATE TABLE @cdmDatabaseSchema.([\w_]*) \(([\s\S]*?)[\n\)];"
 	input_file_contents = read("./assets/ddl/OMOPCDM_duckdb_5.4_ddl.sql", String)
-	all_match_positions = findall(pattern, input_file_contents)
-	all_match_strings = String[input_file_contents[position] for position in all_match_positions]
-	for match in all_match_strings
-		command = replace(match, "@cdmDatabaseSchema." => "")
+	for match in eachmatch(pattern, input_file_contents)
+		command = replace(match.match,
+			"@cdmDatabaseSchema." => "",
+			"integer" => "BIGINT",
+		)
 		DBInterface.execute(con, command)
+		table_name = match.captures |> first |> String
+		DBInterface.execute(con, "COPY $table_name FROM 'assets/omop-mimic-iv/1_omop_data_csv/$table_name.csv';")
 	end
-	DBInterface.execute(con, """EXPORT DATABASE 'Outfiles' (FORMAT CSV, DELIMITER '|');""")
+	#DBInterface.execute(con, """EXPORT DATABASE 'Outfiles' (FORMAT CSV, DELIMITER '|');""")
 end
 
 end # module OmopTestData
