9bfc6a1442ef1ccd57f08c7bd40f36e3ab06ecd2
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-12-19 19:29:47 -0500

    Write to a single .db file

--- src/OmopTestData.jl
+++ src/OmopTestData.jl
@@ -3,19 +3,31 @@ module OmopTestData
 using DuckDB
 
 function main()
+
+	# Create a database in memory
 	con = DBInterface.connect(DuckDB.DB, ":memory:")
+
+	# Create a database output file
+	database_outfile = "out/synthea_omop_test.db"
+	rm(database_outfile, force=true)
+	DBInterface.execute(con, """ ATTACH '$(database_outfile)' AS out_db; """)
+
+	# Parse the ddl into individual commands
 	pattern = r"CREATE TABLE @cdmDatabaseSchema.([\w_]*) \(([\s\S]*?)[\n\)];"
 	input_file_contents = read("./assets/ddl/OMOPCDM_duckdb_5.4_ddl.sql", String)
 	for match in eachmatch(pattern, input_file_contents)
+
+		# Create the table
 		command = replace(match.match,
-			"@cdmDatabaseSchema." => "",
+			"@cdmDatabaseSchema" => "out_db",
 			"integer" => "BIGINT",
 		)
 		DBInterface.execute(con, command)
+
+		# Write the data
 		table_name = match.captures |> first |> String |> uppercase
-		DBInterface.execute(con, "COPY $table_name FROM 'assets/data/Synthea27Nj_5.4/$table_name.csv';")
+		DBInterface.execute(con, "COPY out_db.$table_name FROM 'assets/data/Synthea27Nj_5.4/$table_name.csv';")
 	end
-	DBInterface.execute(con, """EXPORT DATABASE 'Outfiles' (FORMAT CSV, DELIMITER '|');""")
 end
 
 main()
